81e2ba395a7e9ced0fd752a21057b0c95d6bbd14
Exzoz <39750836+Exzoz@users.noreply.github.com>
2018-09-18 20:39:05 -0500

    Add files via upload
    Second code commit

--- Engine.java
+++ Engine.java
@@ -1,3 +1,5 @@
+import hw.GameStatus;
+
 import java.util.Scanner;
 
 public class Engine {
@@ -16,10 +18,46 @@ public class Engine {
         graphics = new Graphics();
     }
 
+    public void initGame() {
+        for (int row = 0; row < 3; row++) {
+            for (int col = 0; col < 3; col++) {
+                board[row][col] = Square.EMPTY; // all squares empty
+            }
+        }
+        gameStatus = GameStatus.PLAYING;
+        currentPlayer = 1;
+    }
+
+    public void start() {
+        graphics.help();
+        System.out.println("Please enter first player name (X):");
+        player1 = input.nextLine();
+        System.out.println("Please enter second payer name (O):");
+        player2 = input.nextLine();
+        initGame();
+        // Play the game
+        do {
+            playerToMove(currentPlayer); // update currentRow and currentCol
+            updateGame(currentPlayer, cRow, cColumn); // update currentState
+            graphics.printBoard(board);
+            //Print message if the game is over
+            if (gameStatus == GameStatus.X_WON) {
+                System.out.println(player1 + "Won!");
+            } else if (gameStatus == GameStatus.O_WON) {
+                System.out.println(player2 + "Won!");
+            } else if (gameStatus == GameStatus.DRAW) {
+                System.out.println("Draw!");
+            }
+            // Switch Player
+            currentPlayer = (currentPlayer == 1) ? 2 : 1;
+        } while (gameStatus == GameStatus.PLAYING); //repeat until
+        }
+    }
 
 
 
 
 
 
-}
+
+
